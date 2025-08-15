488c8f15b743c2766426715033d3ee0dd2ccdb60
Ankit Nandwana <ankit.nandwana091@gmail.com>
2025-08-15 17:58:47 +0530

    This commit refers => add some css and styling for login/signup page, index show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_share_link(file_record)
+    if file_record.shared_link
+      link_to shared_file_url(file_record.shared_link.slug),
+              shared_file_url(file_record.shared_link.slug),
+              target: "_blank"
+    else
+      "Not shared"
+    end
+  end
 end
